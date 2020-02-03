7c10bde403d6e565158a5ea3b703e485cd7be30f
PaperCrafter <leejonghee342@gmail.com>
2020-02-03 23:12:34 +0900

    added dtos

--- server/src/main/java/com/markery/server/models/entity/User.java
+++ server/src/main/java/com/markery/server/models/entity/User.java
@@ -1,4 +1,33 @@
 package com.markery.server.models.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Setter
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String email;
+
+    private String password;
+
+    private boolean verified;
+
+    private String registerdAt;
+
+    private String updatedAt;
+
 }
