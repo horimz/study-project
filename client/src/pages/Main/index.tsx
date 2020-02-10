54e58c35f84536820972c77730c3b7f088a47a1e
David Park <dp.horimz@gmail.com>
2020-02-10 22:03:25 +0900

    update main page layout

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -3,8 +3,37 @@ import React from 'react';
 const _Main: React.FC = () => {
   return (
     <div className='main-content'>
-      <div className='main-content__left-bar'>Left Bar</div>
-      <div className='main-content__content'>Content</div>
+      <div className='main-content__left-bar'>
+        <div className='main-content__left-bar__top'>
+          <div className='main-content__left-bar__logo'>logo</div>
+          <div className='main-content__left-bar__content'>content</div>
+        </div>
+        <div className='main-content__left-bar__bottom'>bottom content</div>
+      </div>
+
+      <div className='main-content__content'>
+        <div className='main-content__content-top'>
+          <div className='main-content__content-top__left'>current folder</div>
+          <div className='main-content__content-top__right'>
+            <div>
+              <input
+                type='text'
+                placeholder='search'
+                className='main-content__content-top__right-search'
+              />
+            </div>
+            <div className='main-content__content-top__right-profile'>
+              <div className='main-content__content-top__right-image'></div>
+            </div>
+          </div>
+        </div>
+        <div className='main-content__content-bottom'>
+          <div className='main-content__content-bottom__left'>
+            folder content (urls, sub dir) scrollable
+          </div>
+          <div className='main-content__content-bottom__right'>right</div>
+        </div>
+      </div>
     </div>
   );
 };
