f8671e88d860d4273ab9af76fb5db34348d41396
David Park <dp.horimz@gmail.com>
2020-02-13 18:10:55 +0900

    add auth middleware

---
+++ express_server/src/controllers/middlewares/authenticateToken.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { keys } from '../../keys';
+import { User, UserInterface } from '../../models/User';
+
+// extend express request interface
+declare global {
+  namespace Express {
+    export interface Request {
+      token: string;
+      user: UserInterface;
+    }
+  }
+}
+
+export async function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = req.header('Authorization');
+
+    if (!authHeader) throw new Error('Authorization header is missing.');
+
+    const token = authHeader.replace('Bearer ', '');
+    const decoded: any = jwt.verify(token, keys.jwtSecret);
+
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token
+    });
+
+    if (!user) throw new Error();
+
+    req.token = token;
+    req.user = user;
+
+    next();
+  } catch (e) {
+    res.status(401).send({ error: 'Authentication error.' });
+  }
+}
