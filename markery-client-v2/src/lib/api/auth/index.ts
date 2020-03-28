5ed08630ba1f7906303e726f2d0cd5bba1e8bed0
David Park <dp.horimz@gmail.com>
2020-03-28 13:14:49 +0900

    Update production url

--- markery-client-v2/src/lib/api/auth/index.ts
+++ markery-client-v2/src/lib/api/auth/index.ts
@@ -2,19 +2,36 @@ import { apiClient } from '../apiClient';
 import { formatRequest } from '../formatRequest';
 import * as authTypes from './types';
 
+let config = {};
+
+if (process.env.NODE_ENV === 'production') {
+  config = {
+    baseURL: process.env.REACT_APP_AUTH_HOST
+  };
+}
+
 export const login = (data: authTypes.LoginInput) =>
-  apiClient.post<authTypes.LoginResponse>(`/auth/login`, formatRequest(data));
+  apiClient.post<authTypes.LoginResponse>(
+    `/auth/login`,
+    formatRequest(data),
+    config
+  );
 
 export const logout = () =>
-  apiClient.post<authTypes.LogoutResponse>(`/auth/logout`, formatRequest({}));
+  apiClient.post<authTypes.LogoutResponse>(
+    `/auth/logout`,
+    formatRequest({}),
+    config
+  );
 
 export const fetchUser = () =>
-  apiClient.get<authTypes.FetchUserResponse>(`/auth/users/confirm`);
+  apiClient.get<authTypes.FetchUserResponse>(`/auth/users/confirm`, config);
 
 export const register = (data: authTypes.RegisterInput) =>
   apiClient.post<authTypes.RegisterResponse>(
     `/auth/users`,
-    formatRequest(data)
+    formatRequest(data),
+    config
   );
 
 export const updateUser = (data: authTypes.UpdateUserInput) =>
